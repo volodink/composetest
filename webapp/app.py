6151865611dd8f319625fb1b39c1c12cfd958733
volodink <volodin.konstantin@gmail.com>
2017-01-08 04:29:06 +0300

    Добавлен первый пример работы с сокетами.

--- webapp/app.py
+++ webapp/app.py
@@ -1,16 +1,33 @@
-from flask import Flask
+import os
+import uuid
 import socket
+
+from flask import Flask, render_template, session
+from flask_socketio import SocketIO, emit
 from redis import Redis
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+socketio = SocketIO(app)
+
 redis = Redis(host='redis', port=6379)
+
+host = socket.gethostname()
+host_ip = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1]
+
+test = 0
+test2 = 0
 
 @app.route('/')
 def hello():
-    count = redis.incr('hits')
-    host = socket.gethostname()
-    host_ip = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1]
-    return 'Hello World! I have been seen {} times.\n So, you hit REDIS for {} times. Server HOST IP={} and HOST={}.\n'.format(count, count, host_ip, host)
+    return render_template('index.html')
+
+
+@socketio.on('connect', namespace='/mcc')
+def ws_conn():
+    c = redis.incr('hits')
+    socketio.emit('hits_count', {'hits_count': c, 'host_ip': host_ip, 'host': host}, namespace='/mcc')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    socketio.run(app, host="0.0.0.0", debug=True)
